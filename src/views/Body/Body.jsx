24c469b3e9b040966c5e095c9fd42a7d7efa261d
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-07-27 21:28:46 +0200

    feat: start main view

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Start } from '../Start/Start';
 import { Route, Routes } from 'react-router-dom'
+import { Main } from "../Main/Main";
 
 export const Body = () => {
   return (
     <>
     <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/game" element={<Main />} />
     </Routes>
     </>
   )
