431652dd508e54fc3eaf15a02d9990fc4f379ec7
Yoana Stamenova <yoana.stamenova@maibornwolff.de>
2025-08-18 16:29:00 +0200

    feat: add questions form

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import "./card.css";
+import "../../data/questions.json"
 
 const Card = () => {
   return (
     <div className='card-body'>
-      <h1>Hello from Card</h1>
+<form>
+  <fieldset>
+    <legend>Question</legend>
+    <div>
+      <input
+        type="radio"
+        id="contactChoice1"
+        name="contact"
+        value="email"
+        defaultChecked />
+      <label htmlFor="contactChoice1">Email</label>
+
+      <input type="radio" id="contactChoice2" name="contact" value="phone" />
+      <label htmlFor="contactChoice2">Phone</label>
+
+      <input type="radio" id="contactChoice3" name="contact" value="mail" />
+      <label htmlFor="contactChoice3">Mail</label>
     </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+  </fieldset>
+</form>
+</div>
   );
 };
 
--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -4,7 +4,7 @@ import Card from '../../components/Card/Card'
 
 export default function Main() {
   return (
-    <div>
+    <div className='main-container'>
       <Card />
     </div>
   )
