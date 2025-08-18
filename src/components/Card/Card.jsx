bcb6e533475284f676135509cde28260b34aba0e
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-08-18 16:33:54 +0200

    fix: tags in card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,30 +5,25 @@ import "../../data/questions.json"
 const Card = () => {
   return (
     <div className='card-body'>
-<form>
-  <fieldset>
-    <legend>Question</legend>
-    <div>
-      <input
-        type="radio"
-        id="contactChoice1"
-        name="contact"
-        value="email"
-        defaultChecked />
-      <label htmlFor="contactChoice1">Email</label>
+        <form>
+          <fieldset>
+            <legend>Question</legend>
+            <div>
+              <input type="radio" id="contactChoice1" name="contact" value="email" defaultChecked />
+              <label htmlFor="contactChoice1">Email</label>
 
-      <input type="radio" id="contactChoice2" name="contact" value="phone" />
-      <label htmlFor="contactChoice2">Phone</label>
+              <input type="radio" id="contactChoice2" name="contact" value="phone" />
+              <label htmlFor="contactChoice2">Phone</label>
 
-      <input type="radio" id="contactChoice3" name="contact" value="mail" />
-      <label htmlFor="contactChoice3">Mail</label>
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-  </fieldset>
-</form>
-</div>
+              <input type="radio" id="contactChoice3" name="contact" value="mail" />
+              <label htmlFor="contactChoice3">Mail</label>
+            </div>
+          <div>
+           <button type="submit">Submit</button>
+          </div>
+          </fieldset>
+        </form>
+      </div>
   );
 };
 
