bfa3352cb1dff36e52d9715bea45c2eb60a243f6
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-08-22 16:37:39 +0200

    feat: add nice styling for end view

--- src/views/End/End.jsx
+++ src/views/End/End.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import './End.css'
+import { useNavigate } from 'react-router-dom'
 
 export const End = () => {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate('/');
+  }
+
+    const result = 3;
+
   return (
-    <div>Thank you for playing!</div>
+    <div className='end-body'>
+      <p className='end-text'>Thank you for playing!</p>
+      <p className='end-score'>{`Here is your score: ${result}`} </p>
+      <button className='end-button' onClick={handleExit}>Exit</button>
+      </div>
   )
 }
