653f9db2c546ad3952f7f98b60a60c9e803c4ee2
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-08-25 14:57:11 +0200

    feat: score added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,80 +5,92 @@ import { useNavigate } from 'react-router-dom';
 import PropTypes from "prop-types";
 
 const Card = ({ onAnswer, onQuestionChange }) => {
-      Card.propTypes = {
-      onAnswer: PropTypes.func,
-      onQuestionChange: PropTypes.func
-      };
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const currentQuestion = questions[currentIndex];
-    let navigate = useNavigate();
+  Card.propTypes = {
+    onAnswer: PropTypes.func,
+    onQuestionChange: PropTypes.func
+  };
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentQuestion = questions[currentIndex];
+  const [score, setScore] = useState(0);
+  let navigate = useNavigate();
 
-    const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [selectedAnswer, setSelectedAnswer] = useState('');
 
-    useEffect(() => {
-      setSelectedAnswer('');
-    }, [currentIndex])
+  const handleSubmit = () => {
+  navigate("/end", { state: { finalScore: score } });
+};
 
-    useEffect(() => {
-  if (onQuestionChange) onQuestionChange(currentIndex);
-}, [currentIndex, onQuestionChange]);
+  useEffect(() => {
+    setSelectedAnswer('');
+  }, [currentIndex])
+
+  useEffect(() => {
+    if (onQuestionChange) onQuestionChange(currentIndex);
+  }, [currentIndex, onQuestionChange]);
 
   return (
     <div className='card-body'>
-        <form>
-           <fieldset>
-           <legend>{currentQuestion.question}</legend>
-             {currentQuestion.answers.map((answer, i) => {
-                const labelClassName =
-                selectedAnswer === answer
+      <form>
+        <fieldset>
+          <legend>{currentQuestion.question}</legend>
+          {currentQuestion.answers.map((answer, i) => {
+            const labelClassName =
+              selectedAnswer === answer
                 ? answer === currentQuestion.correctAnswer
-                ? 'label label--correct'
-                : 'label label--wrong'
+                  ? 'label label--correct'
+                  : 'label label--wrong'
                 : 'label';
 
-               return (
-                 <div key={i}>
-                   <input
-                     type="radio"
-                     id={`answer-${i}`}
-                     name={`q-${currentIndex}`}
-                     value={answer}
-                     onChange={() => {
-                      setSelectedAnswer(answer); 
-                      onAnswer(answer, currentQuestion.correctAnswer)
-                    }}
-                     checked={selectedAnswer === answer}
-                     disabled={!!selectedAnswer} 
-                   />
-                   <label className={labelClassName} htmlFor={`answer-${i}`}>
-                     {answer}
-                   </label>
-                 </div>
-               );
-             })}
+            return (
+              <div key={i}>
+                <input
+                  type="radio"
+                  id={`answer-${i}`}
+                  name={`q-${currentIndex}`}
+                  value={answer}
+                  onChange={() => {
+                    setSelectedAnswer(answer);
+                    if (answer === currentQuestion.correctAnswer) {
+                      setScore(prev => prev + 5);
+                    }
+                    onAnswer(answer, currentQuestion.correctAnswer)
+                  }}
+                  checked={selectedAnswer === answer}
+                  disabled={!!selectedAnswer}
+                />
+                <label className={labelClassName} htmlFor={`answer-${i}`}>
+                  {answer}
+                </label>
+              </div>
+            );
+          })}
           <div>
-          {currentIndex > 0 && (
-          <button className="button-card-back"
-          type="button"
-          onClick={() => { setCurrentIndex(currentIndex - 1);
-          if (onQuestionChange) onQuestionChange(currentIndex - 1)}}> 
-          Back </button> )}
+            {currentIndex > 0 && (
+              <button className="button-card-back"
+                type="button"
+                onClick={() => {
+                  setCurrentIndex(currentIndex - 1);
+                  if (onQuestionChange) onQuestionChange(currentIndex - 1)
+                }}>
+                Back </button>)}
 
-          {currentIndex < questions.length - 1 && (
-             <button className="button-card" type="button"
-            onClick={() => { setCurrentIndex(currentIndex + 1);
-            if (onQuestionChange) onQuestionChange(currentIndex + 1)}}> 
-            Next </button>)}
+            {currentIndex < questions.length - 1 && (
+              <button className="button-card" type="button"
+                onClick={() => {
+                  setCurrentIndex(currentIndex + 1);
+                  if (onQuestionChange) onQuestionChange(currentIndex + 1)
+                }}>
+                Next </button>)}
 
-          {currentIndex === questions.length - 1 && (
-            <button className="button-card-submit" type="submit"
-            onClick={() => navigate("/end")}>
-            Submit
-            </button>)}
+            {currentIndex === questions.length - 1 && (
+              <button className="button-card-submit" type="button"
+                onClick={handleSubmit}>
+                Submit
+              </button>)}
           </div>
-          </fieldset>
-        </form>
-      </div>
+        </fieldset>
+      </form>
+    </div>
   );
 };
 
--- src/views/End/End.jsx
+++ src/views/End/End.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import './End.css'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import "./End.css"
 
 export const End = () => {
   const navigate = useNavigate();
-
-  const handleExit = () => {
-    navigate('/');
-  }
-    const result = 3;
+  const location = useLocation();
+  const finalScore = location.state?.finalScore || 0;
 
   return (
+    <>
     <div className='end-body'>
       <p className='end-text'>Thank you for playing!</p>
-      <p className='end-score'>{`Your final score is: ${result}`} </p>
-      <button className='end-button' onClick={handleExit}>Exit</button>
-      </div>
-  )
-}
+      <p className='end-score'>{`Your final score is: ${finalScore}`}</p>
+      <button className='end-button' onClick={() => navigate('/')}>Exit</button>
+    </div>
+    </>
+  );
+};
