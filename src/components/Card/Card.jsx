631ade22a21e09fef2ed2ac3957f11f494e068c3
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-08-25 14:03:12 +0200

    feat: show confirmation when anwer is selected

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react';
 import "./Card.css";
 import questions from "../../data/questions.json"
 import { useNavigate } from 'react-router-dom';
+import PropTypes from "prop-types";
 
-const Card = () => {
+const Card = ({ onAnswer, onQuestionChange }) => {
+      Card.propTypes = {
+      onAnswer: PropTypes.func,
+      onQuestionChange: PropTypes.func
+      };
     const [currentIndex, setCurrentIndex] = useState(0);
     const currentQuestion = questions[currentIndex];
     let navigate = useNavigate();
@@ -13,6 +18,10 @@ const Card = () => {
     useEffect(() => {
       setSelectedAnswer('');
     }, [currentIndex])
+
+    useEffect(() => {
+  if (onQuestionChange) onQuestionChange(currentIndex);
+}, [currentIndex, onQuestionChange]);
 
   return (
     <div className='card-body'>
@@ -34,7 +43,10 @@ const Card = () => {
                      id={`answer-${i}`}
                      name={`q-${currentIndex}`}
                      value={answer}
-                     onChange={() => setSelectedAnswer(answer)}
+                     onChange={() => {
+                      setSelectedAnswer(answer); 
+                      onAnswer(answer, currentQuestion.correctAnswer)
+                    }}
                      checked={selectedAnswer === answer}
                      disabled={!!selectedAnswer} 
                    />
@@ -48,12 +60,14 @@ const Card = () => {
           {currentIndex > 0 && (
           <button className="button-card-back"
           type="button"
-          onClick={() => setCurrentIndex(currentIndex - 1)}> 
+          onClick={() => { setCurrentIndex(currentIndex - 1);
+          if (onQuestionChange) onQuestionChange(currentIndex - 1)}}> 
           Back </button> )}
 
           {currentIndex < questions.length - 1 && (
              <button className="button-card" type="button"
-            onClick={() => setCurrentIndex(currentIndex + 1)}> 
+            onClick={() => { setCurrentIndex(currentIndex + 1);
+            if (onQuestionChange) onQuestionChange(currentIndex + 1)}}> 
             Next </button>)}
 
           {currentIndex === questions.length - 1 && (
--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import "./Main.css"
-import Card from '../../components/Card/Card'
+import React, { useEffect, useState } from 'react';
+import "./Main.css";
+import Card from '../../components/Card/Card';
 
 export default function Main() {
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState('');
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  useEffect(() => {
+    setSelectedAnswer('');
+    setCorrectAnswer('');
+  }, [questionIndex]);
+
   return (
     <div className='main-body'>
-      <Card />
+      <Card
+        onAnswer={(answer, correct) => {
+          setSelectedAnswer(answer);
+          setCorrectAnswer(correct);
+        }}
+        onQuestionChange={setQuestionIndex}
+      />
+
+      {selectedAnswer && (
+        selectedAnswer === correctAnswer ? (
+          <p className='correct-answer'> ✅ Nice! Your answer is correct! 🥳 </p>
+        ) : (
+          <p className='wrong-answer'> 🚫 Oh no! The correct answer was {correctAnswer} 💡 </p>
+        )
+      )}
     </div>
-  )
+  );
 }
